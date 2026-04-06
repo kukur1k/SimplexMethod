fe72a0696781466f3b6cc40b21136be0aa0fd918
kukur1k <apurkin97@gmail.com>
2026-04-06 22:48:18 +0300

    make max method

--- SimplexMethod/SimplexMethod.cs
+++ SimplexMethod/SimplexMethod.cs
@@ -8,49 +8,91 @@ namespace SimplexMethod
 {
     public class SimplexMethod
     {
-        public static void SimplexCalculateMax(int[,] matrix, int[] targetF)
+        public static void SimplexCalculateMax(double[,] matrix, double[] targetF)
         {
-            //Ограниченияb и значения (x1, x2, x3 ...)
-            List<int> limitsList = new List<int>();
-            for (int i = 0; i < targetF.Length; i++)
+            int rows = matrix.GetLength(0);
+            int colums = matrix.GetLength(1); 
+            
+            
+            //базисные переменные
+            List<int> basis = new List<int>();
+            for (int i = 0; i < rows - 1; i++)
             {
-                if (targetF[i] == 0)
+                basis.Add(targetF.Length + i);
+            }
+            
+            
+            bool needCheck = true;
+            while (needCheck)
+            {
+                
+                //ищем ведущий столбец
+                double minVal = double.MaxValue;  // --------------Целевой столбец
+                int minIdxCol = -1;
+                for (int i = 0; i < colums - 1; i++)
                 {
-                    limitsList.Add(i++);
+                    // проходим по последней строке и ищем минимальный отрицательный
+                    if (matrix[rows - 1, i] < 0 && matrix[rows - 1, i] < minVal)
+                    {
+                        minVal = matrix[rows - 1, i];
+                        minIdxCol = i;
+                    }
+                }
+
+                if (minIdxCol == -1)
+                {
+                    Console.WriteLine("Оптимальное решение -- все значения >= 0");
+                    needCheck = false;
+                    break;
+                }
+                
+                //ищем ведущую строку
+                double minValRow = double.MaxValue; // --------------Целевая строка
+                int minIdxRow = -1;
+                for (int i = 0; i < rows - 1; i++)
+                {
+                    // ищем только положительные, в качестве столбца берем найденный ранее ведущий
+                    if (matrix[i, minIdxCol] > 1e-9)
+                    {
+                        double tempVal = matrix[i, colums - 1] / matrix[i, minIdxCol];
+                        if (tempVal < minVal)
+                        {
+                            minValRow = tempVal;
+                            minIdxRow = i;
+                        }
+                    }
+                }
+                
+                
+                //Меняем в ведущей строке и столбце местами переменые
+
+                basis[minIdxRow] = minIdxCol;
+                
+                //находим переменную
+                double targetVal = matrix[minIdxRow, minIdxCol];
+                
+                //Применяем жордана гауса, делим строку на найденное целевое значение
+                for (int i = 0; i < colums; i++)
+                {
+                    matrix[minIdxRow, i] /= targetVal;
+                }
+                
+                // зануляем столбец
+                for (int i = 0; i < rows; i++)
+                {
+                    // если еще не занулен и если это не ведущая строка
+                    if (i != minIdxRow && Math.Abs(matrix[i, minIdxCol]) > 1e-9)
+                    {
+                        double temp = matrix[i, minIdxCol];
+                        for (int j = 0; j < colums; j++)
+                        {
+                            matrix[i, j] -= temp * matrix[minIdxRow, j];
+                        }
+                    }
                 }
             }
-
-            int[] limits = limitsList.ToArray();
-            int [] values = new int[matrix.GetLength(1)];
-
-            //минимальный отрицательный для Ведущего столбца
-            int minVal = int.MaxValue;
-            int minIdx = 0;
-            for (int i = 0; i < targetF.Length; i++)
-            {
-                if (targetF[i] < 0 && targetF[i] < minVal)
-                {
-                    minVal = targetF[i];
-                    minIdx = i;
-                }
-            }
-
-
-            // пересчет для выбора Ведущей строки
-            double minStringVal = int.MaxValue;
-            int minStringIdx = 0;
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                double tempVal = matrix[-1, minIdx] / matrix[i, minIdx];
-                if (tempVal < minVal && tempVal > 0 && matrix[i, minIdx] != 0)
-                {
-                    minStringVal = tempVal;
-                    minStringIdx = i;
-                }
-            }
-
-
-            //Меняем в ведущей строке и столбце местами переменые
+            
+            Console.WriteLine($"целевая функция ---  {matrix[rows - 1, colums - 1]}");
 
         }
     }
